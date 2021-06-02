179ddedb762a7df62f553a202aa4cd1d37f70cce
YeshwanthKare <yesh.kare@gmail.com>
2021-06-02 19:38:23 +0530

    Project login deploy

--- public/scripts/session.js
+++ public/scripts/session.js
@@ -1,8 +1,17 @@
 function checkIfLoggedIn() {
     const currentToken = localStorage.getItem("token")
-    if(typeof currentToken !== "undefined"){
+
+    if(currentToken === "undefined"){
+        if(!location.href.includes("/pages/login.html") && !location.href.includes("/pages/register.html") && 
+        !location.href.includes("/index.html") && !location.href.includes("/pages/photos.html") &&
+        !location.href.includes("/pages/vectors.html") && !location.href.includes("/pages/illustration.html")  &&
+        !location.href.includes("/pages/individual-image.html")){
+            location.href = "/pages/login.html";
+        }
+    }
+    else if(typeof currentToken === "string"){
         if(location.href.includes("/pages/login.html") || location.href.includes("/pages/register.html")){
-            location.href != "/pages/login.html";
+            location.href = "/pages/login.html";
         }
     }else{
         if(!location.href.includes("/pages/login.html") && !location.href.includes("/pages/register.html") && 
